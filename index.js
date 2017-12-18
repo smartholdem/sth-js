0e6396c5be5787d2aff0358367c9a8cf7dd1da1f
technolog <viktor@allz.su>
2017-12-18 05:52:57 +0300

    js libraries

---
+++ index.js
@@ -0,0 +1,23 @@
+smartholdem = {
+	crypto : require("./lib/transactions/crypto.js"),
+	delegate : require("./lib/transactions/delegate.js"),
+	signature : require("./lib/transactions/signature.js"),
+	multisignature : require("./lib/transactions/multisignature.js"),
+	transaction : require("./lib/transactions/transaction.js"),
+	vote : require("./lib/transactions/vote.js"),
+	ipfs : require("./lib/transactions/ipfs.js"),
+	networks : require("./lib/networks.js"),
+	slots : require("./lib/time/slots.js"),
+
+	ECPair : require("./lib/ecpair.js"),
+	HDNode : require("./lib/hdnode.js"),
+	ECSignature : require("./lib/ecsignature.js"),
+}
+
+// extra aliases for bitcoinlib-js compatibility
+var libCrypto = require('./lib/crypto.js')
+for (var method in libCrypto) {
+	smartholdem.crypto[method] = libCrypto[method]
+}
+
+module.exports = smartholdem;
